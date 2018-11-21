51f0a5028a25f2d438726b99f772c8a2ab533e48
JingqunMa <majingqun@gmail.com>
2018-11-21 10:43:05 -0500

    Generate star index without GTF, zUMIs is flexible and take GTF on the fly

---
+++ idx_noGTF_gen.sh
@@ -0,0 +1,8 @@
+
+
+/home/jingquma/STAR-2.6.1b/bin/Linux_x86_64/STAR \
+--runThreadN 20 \
+--runMode genomeGenerate \
+--genomeDir /home/jingquma/jingquma-extra2/mouse_staridx_noGTF \
+--genomeFastaFiles /home/jingquma/jingquma-extra/mouse_GRCm38.94/GRCm38.94/Mus_musculus.GRCm38.dna.toplevel.fa \
+--limitGenomeGenerateRAM=36524399488
